f40b47f87f985310c4aa55981fd9f2d3851ce074
ddelavega <diegodelavega08@gmail.com>
2018-02-24 06:41:18 -0300

    add email mkt upd rest

---
+++ email-marketing.php
@@ -0,0 +1,15 @@
+<?php include_once("includes/header.php"); ?>
+<body>
+<div id="top" class="main-content">
+<?php include_once("includes/head.php"); ?>
+<h2>Campañas de E-mail Marketing</h2>
+<?php include_once("includes/email-mkt.php"); ?>
+<?php include_once("includes/profile.php"); ?>
+	<div class="top">
+    <a href="#top">TOP</a>
+  </div>
+</div>
+<?php include("includes/js.php"); ?>
+<?php include("includes/ga.php"); ?>
+</body>
+</html>
---
+++ includes/email-mkt.php
@@ -0,0 +1,298 @@
+<?php  ?>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-01.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-02.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-03.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-04.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-05.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-06.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-07.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-08.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-09.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-10.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-11.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-12.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-13.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-14.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-15.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-16.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-17.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-18.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-19.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-20.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-21.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-22.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-23.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-24.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-25.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-26.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-27.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-28.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-29.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-30.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-31.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-32.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-33.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-34.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-35.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-36.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<div class="pics">
+	<span class="pins">
+		<img src="images/email-marketing/email-mkt-37.jpg" alt="DLV | Design" /><br />
+	</span>
+	<div class="txt">
+		<h2><span>e-mail marketing</span></h2>
+	</div>
+</div>
+<?php  ?>
